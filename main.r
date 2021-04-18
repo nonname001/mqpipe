c599e2afee72f94620cba69b02b3a7f37e2cdb47
nonname001 <austin2004@gmail.com>
2021-04-18 13:09:59 -0400

    Added fuzzyjoin package. Also added part 2 of pipeline, which joins sample manifest with data matrix and filters protein groups based on percentage or count of intensities that are NA.

--- main.r
+++ main.r
@@ -4,6 +4,8 @@ library(tidyverse)
 library(dplyr)
 # install.packages("readr")
 library(readr)
+#install.packages("fuzzyjoin")
+library(fuzzyjoin)
 
 # define pipeline
 # pg_df: input dataframe
@@ -11,13 +13,13 @@ library(readr)
 # type: type of intensity to extract (possible values: "MS2", "LFQ", "Raw")
 # log2t: whether intensities are transformed using log2.
 # one_protein_per_row: if set to true, N proteins identified in the same row are split into N different rows.
-runPipeline <- function(pg_df, removeContaminant = TRUE, removeReverse = TRUE, type = "Raw", log2t = FALSE, one_protein_per_row = FALSE) {
+runMQ <- function(pg_df, removeContaminant = TRUE, removeReverse = TRUE, type = "Raw", log2t = FALSE, one_protein_per_row = FALSE) {
   # create copy of input dataframe
   out_df <- tibble(pg_df)
   
   # remove unnecessary columns
   out_df <- out_df %>%
-    select(`Protein IDs`, `Gene names`,starts_with("Intensity"), starts_with("LFQ intensity"), starts_with("MS/MS count"), Reverse, `Potential contaminant`, id, `Peptide IDs`, -Intensity)
+    select(`Protein IDs`, `Gene names`, starts_with("Intensity"), starts_with("LFQ intensity"), starts_with("MS/MS count"), Reverse, `Potential contaminant`, id, `Peptide IDs`, -Intensity)
   
   # perform log2 transformation, if specified
   # 0 is transformed to -Inf
@@ -61,8 +63,85 @@ runPipeline <- function(pg_df, removeContaminant = TRUE, removeReverse = TRUE, t
   return(out_df)
 }
 
+matrix_filter <- function (pg_df, mf_df, sample.id.col='Sample.ID', method='percentage', filterGroup='DIAGNOSIS',percentageNA = 0.5, maxNA=0, intensity_type="Raw", log2t) {
+  # throw error if percentageNA < 0 or > 100
+  if (percentageNA < 0 || percentageNA > 100) {
+    stop("percentageNA must be between 0 and 100.")
+  }
+  
+  # change -Inf to NA
+  is.na(pg_df) <- sapply(pg_df, is.infinite)
+  
+  proteingroups <- tibble(pg_df)
+  
+  # convert protein groups tibble to long format
+  if (intensity_type == "Raw") {
+    proteingroups <- proteingroups %>%
+      gather("Sample", "Intensity", starts_with("Intensity"), starts_with("LFQ intensity"), starts_with("MS/MS count"))
+  } else if (intensity_type == "LFQ") {
+    proteingroups <- proteingroups %>%
+      gather("Sample", "LFQ intensity", starts_with("Intensity"), starts_with("LFQ intensity"), starts_with("MS/MS count"))
+  } else if (intensity_type == "MS2") {
+    proteingroups <- proteingroups %>%
+      gather("Sample", "MS/MS count", starts_with("Intensity"), starts_with("LFQ intensity"), starts_with("MS/MS count"))
+  } else {
+    warning("type should be specified as 'Raw', 'LFQ', or 'MS2'.")
+  }
+  
+  # add regex column to manifest
+  manifest_local <- mf_df %>%
+    mutate(Sample.ID.regex = paste("(Intensity|LFQ intensity|MS/MS count)\\s", Sample.ID, ".*", sep=""))
+  
+  gsub(".", "[.]", manifest_local$Sample.ID.regex)
+  
+  # use left join to add patient diagnosis to sample ID
+  combined_df <- regex_left_join(proteingroups, manifest_local, by=c(Sample = "Sample.ID.regex"))[-ncol(proteingroups) - ncol(manifest_local)] %>%
+    select(-Sample.ID) %>%
+    distinct()
+  
+  # create NARate column names for every unique diagnosis
+  diagnoses <- unique(combined_df$DIAGNOSIS)
+  
+  # filter values that do not meet conditions
+  # first, create columns of NA counts and total diagnoses for each protein group and diagnosis
+  combined_df <- combined_df %>%
+    mutate(MinNARate = 1, MaxNAcount = 0)
+  
+  if (log2t) {
+    for (i in diagnoses) {
+      combined_df <- combined_df %>%
+        group_by(`Protein IDs`) %>%
+        mutate("NACount.{{i}}" := sum(is.na(Intensity) & DIAGNOSIS == i), "NARate.{{i}}" := sum(is.na(Intensity) & DIAGNOSIS == i)/sum(DIAGNOSIS == i), MinNARate = min(MinNARate, sum(is.na(Intensity) & DIAGNOSIS == i)/sum(DIAGNOSIS == i)), MaxNAcount = max(MaxNAcount, sum(is.na(Intensity) & DIAGNOSIS == i)))
+    }
+  } else {
+    for (i in diagnoses) {
+      combined_df <- combined_df %>%
+        group_by(`Protein IDs`) %>%
+        mutate("NACount.{{i}}" := sum(Intensity == 0 & DIAGNOSIS == i), "NARate.{{i}}" := sum(is.na(Intensity) & DIAGNOSIS == i)/sum(DIAGNOSIS == i), MinNARate = min(MinNARate, sum(is.na(Intensity) & DIAGNOSIS == i)/sum(DIAGNOSIS == i)), MaxNAcount = max(MaxNAcount, sum(is.na(Intensity) & DIAGNOSIS == i)))
+    }
+  }
+  
+  # then, filter based on percentage or raw amount
+  if (method == "percentage") {
+    combined_df <- combined_df %>%
+      filter(MinNARate <= percentageNA)
+  } else if (method == "raw") {
+    combined_df <- combined_df %>%
+      group_by(`Protein IDs`, DIAGNOSIS) %>%
+      filter(MaxNAcount <= maxNA)
+  } else {
+    warning("Matrix was not filtered; method was not specified as 'percentage' or 'raw'")
+  }
+  
+  View(combined_df)
+  
+  return(combined_df)
+}
+
 # read protein group files
 pgdata <- read_tsv("pgdata.tsv")
-pgdata_processed <- runPipeline(pgdata)
+manifest <- read_tsv("manifest.tsv")
+pgdata_processed <- runMQ(pgdata, log2t = TRUE)
 # View(pgdata_processed)
-write_tsv(pgdata_processed, "pgdata_processed.tsv")
+# write_tsv(pgdata_processed, "pgdata_processed.tsv")
+pgdata_2 <- matrix_filter(pgdata_processed, manifest, log2t = TRUE, method="raw", maxNA = 5)
